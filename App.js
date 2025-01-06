8d26045f12c09d0d97c4120439d6092df7398373
soyeonkim <soyeonkim@foodtechkorea.com>
2025-01-06 20:45:55 +0900

    insta app tab icon

---
+++ App.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './src/screens/HomeScreen';
+import SearchScreen from './src/screens/SearchScreen';
+import ActivityScreen from './src/screens/ActivityScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+
+import StatusScreen from './src/screens/StatusScreen';
+import FriendProfileScreen from './src/screens/FriendProfileScreen';
+import EditProfileScreen from './src/screens/EditProfileScreen';
+
+import Ionic from 'react-native-vector-icons/Ionicons';
+
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const BottomTabScreen = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarHideOnKeyboard: true,
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: { height: 70 },
+          tabBarIcon: ({focused, size, color}) => {
+            let iconName;
+            color = 'black';
+            if (route.name === 'Home') {
+              iconName = focused ? 'home-sharp' : 'home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'search' : 'search-outline';
+            } else if (route.name === 'Activity') {
+              iconName = focused ? 'heart' : 'heart-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'person-circle' : 'person-circle-outline';
+            }
+            return <Ionic name={iconName} size={size} color={color} />
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+        <Tab.Screen name="Activity" component={ActivityScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    );
+  };
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Bottom" component={BottomTabScreen} />
+        <Stack.Screen name="Status" component={StatusScreen} />
+        <Stack.Screen name="FriendProfile" component={FriendProfileScreen} />
+        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
